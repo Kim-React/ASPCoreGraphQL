1a382c2d9369da5739d44dc1088a2eff9085579a
uitricks <learn.uitricks@gmail.com>
2022-08-27 13:59:42 +0530

    Initial Tech Conference

---
+++ TechConference/Data/Entities/Session.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechConference.Data.Entities
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Room { get; set; }
+
+        public string Day { get; set; }
+
+        public string Format { get; set; }
+
+
+        public string Level { get; set; }
+    }
+}
